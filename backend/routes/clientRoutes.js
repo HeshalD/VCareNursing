afae57c8642d4c7a29d5de0d96688402357c5059
HeshalD <heshal.dissanayake1@gmail.com>
2026-05-04 15:31:36 +0530

    update: MyBookings page bug fixed in frontnend

--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -8,15 +8,15 @@ router.use(protect);
 
 // client profile endpoints
 router.get('/profile/user/:user_id', clientController.getClientProfileByUserId);
+router.get('/active-bookings/:client_id', clientController.getActiveBookingByClientID);
+router.get('/active-bookings', clientController.getActiveBookingByClientID);
+router.get('/all-bookings/:client_id', clientController.getAllBookingsForClient);
+router.get('/all-bookings', clientController.getAllBookingsForClient);
 router.get('/:client_id', clientController.getClientProfile);
 router.patch('/update-me', clientController.updateMe);
 router.delete('/delete-me', clientController.deleteMe);
 
 // bookings endpoints
 router.get('/', protect, restrictTo('SUPER_ADMIN'), clientController.getAllClients);
-router.get('/active-bookings/:client_id', clientController.getActiveBookingByClientID);
-router.get('/active-bookings', clientController.getActiveBookingByClientID);
-router.get('/all-bookings/:client_id', clientController.getAllBookingsForClient);
-router.get('/all-bookings', clientController.getAllBookingsForClient);
 
 module.exports = router;
